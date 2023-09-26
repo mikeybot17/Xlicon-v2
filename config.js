547ba9424957a58392816fcfe166c64785b931ac
mikeybot17 <146141166+mikeybot17@users.noreply.github.com>
2023-09-27 01:53:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801853262586","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801853262586","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801928502256"];
-global.OwnerName = "Slasher";
-global.BotName = "XLICON-MD";
-global.packname = "X-2.0 Bot";                             //Do not change.
-global.author = "By: Slasher";                               //Do not change.
+global.Owner = ["201112422078","201202543438"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["201112422078","201202543438"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["201112422078"];
+global.OwnerName = "mikey-Nour";
+global.BotName = "ورق عنب-MD";
+global.packname = " ورق عنب بوت";                             //Do not change.
+global.author = "By: mikey-Nour;                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/Xlicon-v2"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/EjsQvJNcFGVCSfaBEIxZm2"; 
 
